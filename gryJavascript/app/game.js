c88b7294145602d5abd18a36962a1c62cb46f5ad
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-07-04 22:01:24 +0200

    d

--- gryJavascript/app/game.js
+++ gryJavascript/app/game.js
@@ -1,8 +1,28 @@
 class Game {
-    start(){
-        console.log('Lets play the game');
-    }
+  constructor({
+    lettersWrapper,
+    categoryWrapper,
+    wordWrapper,
+    outputWrapper,
+  }) {
+      this.lettersWrapper = lettersWrapper
+      this.categoryWrapper = categoryWrapper
+      this.wordWrapper = wordWrapper
+      this.outputWrapper = outputWrapper
+  }
+  start() {
+    console.log("Lets play the game");
+  }
 }
 
-const game = new Game();
-game.start()
+const Damian = {
+    name: 'Damian',
+    age: 26,
+
+}
+
+function showName({name, age}){
+    console.log(name, age);
+}
+
+showName(Damian);
