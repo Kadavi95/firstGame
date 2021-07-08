4328287ce3832031d065c6bf05cc36e39843ec47
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-07-09 00:25:45 +0200

    update

--- gryJavascript/app/game.js
+++ gryJavascript/app/game.js
@@ -1,28 +1,66 @@
+import { Quote} from './Quote.js';
 class Game {
-  constructor({
-    lettersWrapper,
-    categoryWrapper,
-    wordWrapper,
-    outputWrapper,
-  }) {
-      this.lettersWrapper = lettersWrapper
-      this.categoryWrapper = categoryWrapper
-      this.wordWrapper = wordWrapper
-      this.outputWrapper = outputWrapper
+  quotes = [
+    {
+      text: "Pan Tadeusz",
+      category: "Utwór literacki",
+    },
+    { text: "janko muzykant", 
+    category: "Utwór literacki" },
+    { text: "akademia pana kleksa",
+    category: "Film" },
+    { text: "matrix", 
+    category: "Film" },
+  ];
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+
+    const {text, category} = this.quotes[Math.floor(Math.random()* this.quotes.length)]
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text); 
+    
   }
+  guess(letter, event) {
+    event.target.disabled = true;
+    this.quote.guess(letter);
+    this.drawQuote();
+
+  }
+
+  drawLetters(){
+    const labelTable = [];
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      labelTable.push(label);
+     
+    }
+    console.log(labelTable);
+    labelTable.forEach((item) => {
+      const button = document.createElement("button")
+      button.innerHTML = item;
+      button.addEventListener("click", (event) => {
+        this.guess(item, event);
+      });
+      this.lettersWrapper.appendChild(button);
+    });
+  }
+  drawQuote(){
+    const content = this.quote.getContent();
+    this.wordWrapper.innerHTML = content
+  }      
   start() {
-    console.log("Lets play the game");
+    this.drawLetters();
+    this.drawQuote();
   }
 }
+const game = new Game({
+    lettersWrapper: document.getElementById('letters'),
+    categoryWrapper: document.getElementById('category'),
+    wordWrapper: document.getElementById('word'),
+    outputWrapper: document.getElementById('output')  
+});
 
-const Damian = {
-    name: 'Damian',
-    age: 26,
-
-}
-
-function showName({name, age}){
-    console.log(name, age);
-}
-
-showName(Damian);
+game.start()
